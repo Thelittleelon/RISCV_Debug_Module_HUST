958942bb573c8c3effd6c9afe3c3d17ab9eb22f9
Thelittleelon <hdangdepzai@gmail.com>
2025-05-08 08:52:08 +0700

    Change in coding style

--- rtl/dtm_cdc.sv
+++ rtl/dtm_cdc.sv
@@ -25,7 +25,9 @@ module dtm_cdc (
   input  logic             dbg_dmi_resp_valid_i
 );
 
-  // dmi_clear sync
+  // ---------------------------------------------------------------------------
+  // Synchronize dmi_clear signal (JTAG -> System domain)
+  // ---------------------------------------------------------------------------
   logic [1:0] dmi_clear_sync;
   always_ff @(posedge clk or negedge rst_ni) begin
     if (!rst_ni)
@@ -35,50 +37,77 @@ module dtm_cdc (
   end
   assign dbg_dmi_clear_o = dmi_clear_sync[1];
 
-  // Mid wires
-  dm::dmi_req_t     dmi_req_mid;
-  logic             dmi_req_valid_mid;
-  logic             dmi_req_ready_mid;
+  // ---------------------------------------------------------------------------
+  // Internal intermediate wires
+  // ---------------------------------------------------------------------------
+  // Request path
+  dm::dmi_req_t dmi_req_mid;
+  logic         dmi_req_valid_mid, dmi_req_ready_mid;
 
-  dm::dmi_resp_t    dmi_resp_mid_1;
-  logic             dmi_resp_valid_mid_1;
-  logic             dmi_resp_ready_mid_1;
+  // Response path
+  dm::dmi_resp_t dmi_resp_mid_1, dmi_resp_mid_2;
+  logic          dmi_resp_valid_mid_1, dmi_resp_ready_mid_1;
+  logic          dmi_resp_valid_mid_2, dmi_resp_ready_mid_2;
 
-  dm::dmi_resp_t    dmi_resp_mid_2;
-  logic             dmi_resp_valid_mid_2;
-  logic             dmi_resp_ready_mid_2;  
+  // ---------------------------------------------------------------------------
+  // Request Synchronization (TCK → CLK)
+  // ---------------------------------------------------------------------------
+  cdc_stage_req stage_req_0 (
+    .clk_i           (clk),
+    .rst_ni          (rst_ni),
+    .dmi_req_i       (jtag_dmi_req_i),
+    .dmi_req_valid_i (jtag_dmi_req_valid_i),
+    .dmi_req_ready_o (jtag_dmi_req_ready_o),
+    .dmi_req_o       (dmi_req_mid),
+    .dmi_req_valid_o (dmi_req_valid_mid),
+    .dmi_req_ready_i (dmi_req_ready_mid)
+  );
 
-  // Request synchronizer
-  cdc_stage_req stage_req_0 (.clk_i(clk), .rst_ni(rst_ni),
-    .dmi_req_i(jtag_dmi_req_i), .dmi_req_valid_i(jtag_dmi_req_valid_i),
-    .dmi_req_ready_o(jtag_dmi_req_ready_o),
-    .dmi_req_o(dmi_req_mid), .dmi_req_valid_o(dmi_req_valid_mid),
-    .dmi_req_ready_i(dmi_req_ready_mid));
+  cdc_stage_req stage_req_1 (
+    .clk_i           (clk),
+    .rst_ni          (rst_ni),
+    .dmi_req_i       (dmi_req_mid),
+    .dmi_req_valid_i (dmi_req_valid_mid),
+    .dmi_req_ready_o (dmi_req_ready_mid),
+    .dmi_req_o       (dbg_dmi_req_o),
+    .dmi_req_valid_o (dbg_dmi_req_valid_o),
+    .dmi_req_ready_i (dbg_dmi_req_ready_i)
+  );
 
-  cdc_stage_req stage_req_1 (.clk_i(clk), .rst_ni(rst_ni),
-    .dmi_req_i(dmi_req_mid), .dmi_req_valid_i(dmi_req_valid_mid),
-    .dmi_req_ready_o(dmi_req_ready_mid),
-    .dmi_req_o(dbg_dmi_req_o), .dmi_req_valid_o(dbg_dmi_req_valid_o),
-    .dmi_req_ready_i(dbg_dmi_req_ready_i));
+  // ---------------------------------------------------------------------------
+  // Response Synchronization (CLK → TCK)
+  // ---------------------------------------------------------------------------
+  cdc_stage_resp stage_resp_0 (
+    .tck_i            (tck),
+    .trst_ni          (trst_ni),
+    .dmi_resp_i       (dbg_dmi_resp_i),
+    .dmi_resp_valid_i (dbg_dmi_resp_valid_i),
+    .dmi_resp_ready_o (dbg_dmi_resp_ready_o),
+    .dmi_resp_o       (dmi_resp_mid_1),
+    .dmi_resp_valid_o (dmi_resp_valid_mid_1),
+    .dmi_resp_ready_i (dmi_resp_ready_mid_1)
+  );
 
-  // Response synchronizer
-  cdc_stage_resp stage_resp_0 (.tck_i(tck), .trst_ni(trst_ni),
-    .dmi_resp_i(dbg_dmi_resp_i), .dmi_resp_valid_i(dbg_dmi_resp_valid_i),
-    .dmi_resp_ready_o(dbg_dmi_resp_ready_o),
-    .dmi_resp_o(dmi_resp_mid_1), .dmi_resp_valid_o(dmi_resp_valid_mid_1),
-    .dmi_resp_ready_i(dmi_resp_ready_mid_1));
+  cdc_stage_resp stage_resp_1 (
+    .tck_i            (tck),
+    .trst_ni          (trst_ni),
+    .dmi_resp_i       (dmi_resp_mid_1),
+    .dmi_resp_valid_i (dmi_resp_valid_mid_1),
+    .dmi_resp_ready_o (dmi_resp_ready_mid_1),
+    .dmi_resp_o       (dmi_resp_mid_2),
+    .dmi_resp_valid_o (dmi_resp_valid_mid_2),
+    .dmi_resp_ready_i (dmi_resp_ready_mid_2)
+  );
 
-  cdc_stage_resp stage_resp_1 (.tck_i(tck), .trst_ni(trst_ni),
-    .dmi_resp_i(dmi_resp_mid_1), .dmi_resp_valid_i(dmi_resp_valid_mid_1),
-    .dmi_resp_ready_o(dmi_resp_ready_mid_1),
-    .dmi_resp_o(dmi_resp_mid_2), .dmi_resp_valid_o(dmi_resp_valid_mid_2),
-    .dmi_resp_ready_i(dmi_resp_ready_mid_2));
-
-
-  cdc_stage_resp stage_resp_2 (.tck_i(tck), .trst_ni(trst_ni),
-    .dmi_resp_i(dmi_resp_mid_2), .dmi_resp_valid_i(dmi_resp_valid_mid_2),
-    .dmi_resp_ready_o(dmi_resp_ready_mid_2),
-    .dmi_resp_o(jtag_dmi_resp_o), .dmi_resp_valid_o(jtag_dmi_resp_valid_o),
-    .dmi_resp_ready_i(jtag_dmi_resp_ready_i));    
+  cdc_stage_resp stage_resp_2 (
+    .tck_i            (tck),
+    .trst_ni          (trst_ni),
+    .dmi_resp_i       (dmi_resp_mid_2),
+    .dmi_resp_valid_i (dmi_resp_valid_mid_2),
+    .dmi_resp_ready_o (dmi_resp_ready_mid_2),
+    .dmi_resp_o       (jtag_dmi_resp_o),
+    .dmi_resp_valid_o (jtag_dmi_resp_valid_o),
+    .dmi_resp_ready_i (jtag_dmi_resp_ready_i)
+  );
 
 endmodule
