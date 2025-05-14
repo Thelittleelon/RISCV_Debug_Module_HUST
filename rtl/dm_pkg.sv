eabfe0909c8ce55b001aad5804d0d83703def789
Thelittleelon <hdangdepzai@gmail.com>
2025-05-14 17:16:34 +0700

    Update

--- rtl/dm_pkg.sv
+++ rtl/dm_pkg.sv
@@ -1,3 +1,21 @@
+/* Copyright 2018 ETH Zurich and University of Bologna.
+ * Copyright and related rights are licensed under the Solderpad Hardware
+ * License, Version 0.51 (the “License”); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+ * or agreed to in writing, software, hardware and materials distributed under
+ * this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * File:   dm_pkg.sv
+ * Author: Florian Zaruba <zarubaf@iis.ee.ethz.ch>
+ * Date:   30.6.2018
+ *
+ * Description: Debug-module package, contains common system definitions.
+ *
+ */
+
 package dm;
   localparam logic [3:0] DbgVersion013 = 4'h2;
   // size of program buffer in junks of 32-bit words
